a808fc70eafb8980fd59ebe012fe47752559596b
henryoier <henryoier@gmail.com>
2026-09-15 16:00:15 -0700

    Append 38 still photos to the SF Glens gallery

--- album.js
+++ album.js
@@ -4,7 +4,7 @@ if (albumPage) {
   const media = window.CALBLUE_MEDIA || {};
   const mediaBaseUrl = (media.baseUrl || '').replace(/\/$/, '');
   const albums = {
-    'swpl-sf-glens': { opponent: 'SF Glens', date: 'September 13, 2026', count: 61, competition: '2026 SWPL Pacific Premier League' },
+    'swpl-sf-glens': { opponent: 'SF Glens', date: 'September 13, 2026', count: 99, competition: '2026 SWPL Pacific Premier League' },
     tiger: { opponent: 'Tiger', date: 'May 24, 2026', count: 135, competition: '2026 NCCSF' },
     nbh: { opponent: 'NBH', date: 'May 23, 2026', count: 58, competition: '2026 NCCSF' },
     sfu: { opponent: 'SFU', date: 'May 16, 2026', count: 24, competition: '2026 NCCSF' },
